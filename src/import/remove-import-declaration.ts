7e27888e7184220ca32007c4b6680a566737a1bb
linbudu599 <linbudu599@gmail.com>
2021-08-12 21:32:02 +0800

    import: remove imports

---
+++ src/import/remove-import-declaration.ts
@@ -0,0 +1,59 @@
+import { SourceFile } from "ts-morph";
+import { getImportDeclaration } from "./get-import-declaration";
+import {
+  importDeclarationExist,
+  hasImportDeclaration,
+} from "./has-import-declaration";
+import {
+  checkIsDefaultImportByDeclaration,
+  checkIsNamespaceImportByDeclaration,
+  checkIsNamedImportByDeclaration,
+} from "./check-import-type";
+
+export function removeImportDeclaration(
+  source: SourceFile,
+  specifiers: string[],
+  apply = true
+) {
+  const validSpecToRemove = specifiers.filter((spec) =>
+    importDeclarationExist(source, spec)
+  );
+
+  validSpecToRemove.forEach((spec) => {
+    const targetImport = getImportDeclaration(source, spec)!;
+
+    targetImport.remove();
+  });
+
+  apply && source.saveSync();
+}
+
+export function removeImportDeclarationByTypes(
+  source: SourceFile,
+  removeByTypes?: Partial<Record<"namespace" | "default" | "named", boolean>>,
+  apply = true
+) {
+  if (!hasImportDeclaration(source)) {
+    return;
+  }
+  const sourceImports = getImportDeclaration(source);
+
+  sourceImports.forEach((imp) => {
+    if (removeByTypes?.default && checkIsDefaultImportByDeclaration(imp)) {
+      imp.remove();
+      return;
+    }
+
+    if (removeByTypes?.named && checkIsNamedImportByDeclaration(imp)) {
+      imp.remove();
+      return;
+    }
+
+    if (removeByTypes?.namespace && checkIsNamespaceImportByDeclaration(imp)) {
+      imp.remove();
+      return;
+    }
+  });
+
+  apply && source.saveSync();
+}
